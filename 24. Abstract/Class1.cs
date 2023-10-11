acb800b0f05f28d74b53d780a055e643b8bacfbc
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-11 07:45:17 +0200

    Add project files.

---
+++ 24. Abstract/Class1.cs
@@ -0,0 +1,18 @@
+﻿using _24.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abstract
+{
+    abstract class Vehicle
+    {
+        public int wheels = 4;
+
+    }
+}     class corsa : Vehicle
+      {
+           public int spareweels = 1;
+      }
---
+++ 24. Abstract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // vehicle car1 = new vwhicle();
+            corsa car2 = new corsa();
+            Console.WriteLine(car2.wheels);
+
+            Console.ReadLine();
+        }
+    }
+}
